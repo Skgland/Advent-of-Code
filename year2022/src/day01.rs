a725aec069e4424fd349cc111d9cf9508a271b59
Bennet Bleßmann <bb-github@t-online.de>
2022-12-01 19:33:57 +0100

    prep for 2022

---
+++ year2022/src/day01.rs
@@ -0,0 +1,31 @@
+pub fn part1(input: &str) -> u32 {
+    // TODO
+}
+
+pub fn part2(input: &str) -> u32 {
+    // TODO
+}
+
+#[test]
+fn part1_example() {
+    let input = include_str!("../input/day01.example.txt");
+    assert_eq!(part1(input), todo!());
+}
+
+#[test]
+fn part1_full() {
+    let input = include_str!(concat!("../input/day01.txt"));
+    assert_eq!(part1(input), todo!());
+}
+
+#[test]
+fn part2_example() {
+    let input = include_str!("../input/day01.example.txt");
+    assert_eq!(part2(input), todo!());
+}
+
+#[test]
+fn part2_full() {
+    let input = include_str!(concat!("../input/day01.txt"));
+    assert_eq!(part2(input), todo!());
+}
