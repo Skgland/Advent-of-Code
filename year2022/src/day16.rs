5d3cf94118a8e832e110f8be55092a893f172c7c
Bennet Bleßmann <bb-github@t-online.de>
2022-12-01 20:45:00 +0100

    uncomment code again

--- year2022/src/day16.rs
+++ year2022/src/day16.rs
@@ -1,29 +1,33 @@
 pub fn part1(input: &str) -> u32 {
-    // TODO
+    todo!()
 }
 
 pub fn part2(input: &str) -> u32 {
-    // TODO
+    todo!()
 }
 
+#[ignore = "not yet implemented"]
 #[test]
 fn part1_example() {
     let input = include_str!("../input/day16.example.txt");
     assert_eq!(part1(input), todo!());
 }
 
+#[ignore = "not yet implemented"]
 #[test]
 fn part1_full() {
     let input = include_str!(concat!("../input/day16.txt"));
     assert_eq!(part1(input), todo!());
 }
 
+#[ignore = "not yet implemented"]
 #[test]
 fn part2_example() {
     let input = include_str!("../input/day16.example.txt");
     assert_eq!(part2(input), todo!());
 }
 
+#[ignore = "not yet implemented"]
 #[test]
 fn part2_full() {
     let input = include_str!(concat!("../input/day16.txt"));
