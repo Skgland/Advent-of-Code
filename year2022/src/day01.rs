5bccbd377fc1944a0ac5ecff2aa85e110af3c1a1
Bennet Bleßmann <bb-github@t-online.de>
2022-12-01 20:14:37 +0100

    partake in day one of aoc

--- year2022/src/day01.rs
+++ year2022/src/day01.rs
@@ -1,35 +1,52 @@
+fn both(input: &str, top: usize) -> u32 {
+    let mut calories = input.lines().map(|line| line.parse::<u32>());
+    let mut current = 0;
+    let mut elfs = vec![];
+
+    while let Some(val) = calories.next() {
+        if let Ok(val) = val {
+            current += val;
+        } else {
+            elfs.push(current);
+            current = 0;
+        }
+    }
+    elfs.push(current);
+
+    elfs.sort();
+    elfs.reverse();
+
+    dbg!(&elfs[..top]).into_iter().sum()
+}
+
 pub fn part1(input: &str) -> u32 {
-    todo!()
+    both(input, 1)
 }
 
 pub fn part2(input: &str) -> u32 {
-    todo!()
+    both(input, 3)
 }
 
-#[ignore = "not yet implemented"]
 #[test]
 fn part1_example() {
     let input = include_str!("../input/day01.example.txt");
-    assert_eq!(part1(input), todo!());
+    assert_eq!(part1(input), 24000);
 }
 
-#[ignore = "not yet implemented"]
 #[test]
 fn part1_full() {
     let input = include_str!(concat!("../input/day01.txt"));
-    assert_eq!(part1(input), todo!());
+    assert_eq!(part1(input), 70720);
 }
 
-#[ignore = "not yet implemented"]
 #[test]
 fn part2_example() {
     let input = include_str!("../input/day01.example.txt");
-    assert_eq!(part2(input), todo!());
+    assert_eq!(part2(input), 45000);
 }
 
-#[ignore = "not yet implemented"]
 #[test]
 fn part2_full() {
     let input = include_str!(concat!("../input/day01.txt"));
-    assert_eq!(part2(input), todo!());
+    assert_eq!(part2(input), 207148);
 }
