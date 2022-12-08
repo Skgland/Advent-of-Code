2dc1a6be97b342fafff5ed0e08b44e6eb761fb0e
Skgland <bennet.blessmann+github@googlemail.com>
2022-12-08 20:37:22 +0100

    fix formatting

--- year2022/src/day07.rs
+++ year2022/src/day07.rs
@@ -1,21 +1,19 @@
-
 use std::collections::HashMap;
 
-
-enum DirOrFile<'a>{
+enum DirOrFile<'a> {
     Dir(Dir<'a>),
     File(usize),
 }
 
-struct Dir <'a>{
-    content: HashMap<&'a [u8], DirOrFile<'a>>
+struct Dir<'a> {
+    content: HashMap<&'a [u8], DirOrFile<'a>>,
 }
 
 impl<'a> Dir<'a> {
     fn find_folder(&mut self, input: &[&'a [u8]]) -> &mut Dir<'a> {
         let mut cwd = self;
         let mut tmp = input;
-        while let [parent,rem@..] = tmp {
+        while let [parent, rem @ ..] = tmp {
             tmp = rem;
             match cwd.content.get_mut(parent).unwrap() {
                 DirOrFile::Dir(dir) => cwd = dir,
@@ -25,39 +23,46 @@ impl<'a> Dir<'a> {
         cwd
     }
     fn mkdir(&mut self, input: &[&'a [u8]], child: &'a [u8]) {
-        self.find_folder(input).content.insert(child, DirOrFile::Dir(Dir{ content: Default::default() }));
+        self.find_folder(input).content.insert(
+            child,
+            DirOrFile::Dir(Dir {
+                content: Default::default(),
+            }),
+        );
     }
 
     fn touch(&mut self, input: &[&'a [u8]], child: &'a [u8], size: usize) {
-        self.find_folder(input).content.insert(child, DirOrFile::File(size));
+        self.find_folder(input)
+            .content
+            .insert(child, DirOrFile::File(size));
     }
 
     fn recursive_size(&self) -> usize {
-        self.content.iter().map(|(_,elem)| match elem {
-            DirOrFile::Dir(dir) => dir.recursive_size(),
-            DirOrFile::File(size) => *size,
-        }).sum()
+        self.content
+            .iter()
+            .map(|(_, elem)| match elem {
+                DirOrFile::Dir(dir) => dir.recursive_size(),
+                DirOrFile::File(size) => *size,
+            })
+            .sum()
     }
 
-    fn walk_dirs(&self, walk_dir: &mut impl FnMut(&Dir<'a>) ) {
+    fn walk_dirs(&self, walk_dir: &mut impl FnMut(&Dir<'a>)) {
         walk_dir(self);
         for (name, entry) in &self.content {
             match entry {
                 DirOrFile::Dir(dir) => dir.walk_dirs(walk_dir),
-                DirOrFile::File(size) => {},
+                DirOrFile::File(size) => {}
             }
         }
     }
 }
 
-
-
-
-
-fn parse(input: &str) -> Dir
-{
+fn parse(input: &str) -> Dir {
     let mut cwd = vec![];
-    let mut fs = Dir{content: Default::default()};
+    let mut fs = Dir {
+        content: Default::default(),
+    };
 
     for line in input.lines() {
         match line.as_bytes() {
@@ -67,15 +72,13 @@ fn parse(input: &str) -> Dir
             [b'$', b' ', b'c', b'd', b' ', b'.', b'.'] => {
                 cwd.pop();
             }
-            [b'$', b' ', b'c', b'd', b' ', path@..] => {
+            [b'$', b' ', b'c', b'd', b' ', path @ ..] => {
                 cwd.push(path);
             }
             [b'$', b' ', b'l', b's'] => {}
-            [b'd', b'i', b'r', b' ', path@..] => {
-                fs.mkdir(&cwd, path)
-            }
+            [b'd', b'i', b'r', b' ', path @ ..] => fs.mkdir(&cwd, path),
             file => {
-                let (size, name) =  line.split_once(' ').unwrap();
+                let (size, name) = line.split_once(' ').unwrap();
                 fs.touch(&cwd, name.as_bytes(), size.parse().unwrap())
             }
         }
@@ -87,23 +90,24 @@ pub fn part1(input: &str) -> usize {
     let fs = parse(input);
     let mut total = 0;
     fs.walk_dirs(&mut |dir| {
-        let size =dir.recursive_size();
+        let size = dir.recursive_size();
         if size <= 100_000 {
             total += size
-        }});
+        }
+    });
     total
 }
 
 pub fn part2(input: &str) -> usize {
     let fs = parse(input);
-    const TOTAL_SPACE : usize= 70000000;
-    const REQUIRED_SPACE : usize = 30000000;
+    const TOTAL_SPACE: usize = 70000000;
+    const REQUIRED_SPACE: usize = 30000000;
     let used_space = fs.recursive_size();
     let free_space = TOTAL_SPACE - used_space;
     let missing_space = REQUIRED_SPACE - free_space;
 
     let mut min = TOTAL_SPACE;
-    fs.walk_dirs(&mut  |dir| {
+    fs.walk_dirs(&mut |dir| {
         let size = dir.recursive_size();
         if size >= missing_space && size < min {
             min = size;
@@ -125,13 +129,11 @@ fn part1_full() {
     assert_eq!(part1(input), 2104783);
 }
 
-
 #[test]
 fn part2_example() {
     let input = include_str!("../input/day07.example.txt");
     assert_eq!(part2(input), 24933642);
 }
-
 
 #[test]
 fn part2_full() {
