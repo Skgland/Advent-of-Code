b30c18f79169343fac8740c311787289e32a7a10
Skgland <bennet.blessmann+github@googlemail.com>
2023-10-21 23:35:03 +0200

    AoC 2015 Day 4

--- year2015/src/day04.rs
+++ year2015/src/day04.rs
@@ -1,38 +1,47 @@
-fn parse_input(input: &str) -> impl Iterator<Item = u32> + '_ {
-    todo!("parse_input WIP");
-    std::iter::empty()
+use md5::digest::Digest;
+
+#[inline(always)]
+fn find_md5_prefix(input: &str, prefix: &str) -> u32 {
+    let input = input.trim();
+    let md5_prefix = md5::Md5::new_with_prefix(input.as_bytes());
+    (1..)
+        .find(|suf| {
+            let digest = md5_prefix.clone().chain_update(format!("{suf}")).finalize();
+            let hash = format!("{:x}", digest);
+            hash.starts_with(prefix)
+        })
+        .unwrap()
 }
 
 pub fn part1(input: &str) -> u32 {
-    let mut iter = parse_input(input);
-    todo!("part1 WIP")
+    find_md5_prefix(input, "00000")
 }
 
 pub fn part2(input: &str) -> u32 {
-    let mut iter = parse_input(input);
-    todo!("part2 WIP")
+    find_md5_prefix(input, "000000")
 }
 
 #[test]
-fn part1_example() {
-    let input = include_str!(concat!("../input/day04.example.txt"));
-    assert_eq!(part1(input), 7);
+fn part1_example1() {
+    let input = "abcdef";
+    assert_eq!(part1(input), 609043);
+}
+
+#[test]
+fn part1_example2() {
+    let input = "pqrstuv";
+    assert_eq!(part1(input), 1048970);
 }
 
 #[test]
 fn part1_full() {
     let input = include_str!(concat!("../input/day04.txt"));
-    assert_eq!(part1(input), 1292);
+    assert_eq!(part1(input), 282749);
 }
 
-#[test]
-fn part2_example() {
-    let input = include_str!("../input/day04.example.txt");
-    assert_eq!(part2(input), 5);
-}
-
+#[ignore = "too slow"]
 #[test]
 fn part2_full() {
     let input = include_str!(concat!("../input/day04.txt"));
-    assert_eq!(part2(input), 1262);
+    assert_eq!(part2(input), 9962624);
 }
