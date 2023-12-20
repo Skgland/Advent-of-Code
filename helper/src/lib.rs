c1a2cf902fd1b0c6cd0917c3bd0430905f57a635
Skgland <bennet.blessmann+github@googlemail.com>
2023-12-20 21:52:46 +0100

    AoC 2023 Day 20

--- helper/src/lib.rs
+++ helper/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn lcm(a: usize, b: usize) -> usize {
+use std::ops::{Rem, Div, Mul};
+
+pub trait Zero {
+    const ZERO : Self;
+}
+
+macro_rules! impl_zero {
+    ($($t:ty),+) => {
+        $(
+            impl Zero for $t {
+                const ZERO: Self = 0;
+            }
+        )*
+    };
+}
+
+impl_zero!(u8, u16, u32, u64, u128, usize);
+
+pub fn lcm<T>(a: T, b: T) -> T  where T: Copy + Ord + Rem<Output = T> + Div<Output = T> + Zero + Mul<Output = T>{
     a / gcd(a, b) * b
 }
 
-pub fn gcd(width: usize, height: usize) -> usize {
+pub fn gcd<T>(width: T, height: T) -> T where T: Copy + Ord + Rem<Output = T> + Zero {
     let mut a = width;
     let mut b = height;
     if b > a {
         std::mem::swap(&mut a, &mut b);
     }
-    while b != 0 {
+    while b != Zero::ZERO {
         (a, b) = (b, a % b);
     }
     a
