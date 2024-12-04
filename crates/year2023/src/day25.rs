bcbc7f632a61d9d588ab02fc4e47744dbb695d38
Bennet Bleßmann <bb-github@t-online.de>
2024-12-05 00:09:57 +0100

    parse AoC 2023 Day 25

--- crates/year2023/src/day25.rs
+++ crates/year2023/src/day25.rs
@@ -1,16 +1,30 @@
-fn parse_input(input: &str) -> impl Iterator<Item = u32> + '_ {
-    todo!("parse_input WIP");
-    std::iter::empty()
+use std::collections::{BTreeMap, HashSet};
+
+#[derive(Debug)]
+struct Graph<'a> {
+    nodes: BTreeMap<&'a str, HashSet<&'a str>>,
+}
+
+fn parse_input(input: &str) -> Graph<'_> {
+    Graph {
+        nodes: input.lines().fold(BTreeMap::new(), |mut acc, next| {
+            let (src, dests) = next.split_once(": ").unwrap();
+            for dest in dests.split(' ') {
+                acc.entry(src).or_default().insert(dest);
+            }
+            acc
+        }),
+    }
 }
 
 pub fn part1(input: &str) -> u32 {
-    let mut iter = parse_input(input);
-    todo!("part1 WIP")
+    let mut graph = parse_input(input);
+    todo!("part1 WIP: {graph:?}")
 }
 
 pub fn part2(input: &str) -> u32 {
-    let mut iter = parse_input(input);
-    todo!("part2 WIP")
+    let mut graph = parse_input(input);
+    todo!("part2 WIP: {graph:?}")
 }
 
 #[test]
