583e474c1c85881a8c696ae869f47f2fb5248682
Skgland <bennet.blessmann+github@googlemail.com>
2025-12-11 20:08:07 +0100

    AoC 2025 Day 11 Part 2

--- crates/year2025/src/day11.rs
+++ crates/year2025/src/day11.rs
@@ -14,6 +14,13 @@ const INPUT_EXAMPLE1: &str = include_str!(concat!(
     "/../../inputs/example/year2025/day11.example1.txt"
 ));
 
+
+#[cfg(test)]
+const INPUT_EXAMPLE2: &str = include_str!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/../../inputs/example/year2025/day11.example2.txt"
+));
+
 #[distributed_slice(TASKS)]
 static PART1: Task = Task {
     path: &["2025", "11", "part1"],
@@ -36,16 +43,14 @@ fn parse_input(input: &str) -> BTreeMap<&str, BTreeSet<&str>> {
     }).collect()
 }
 
-pub fn part1(input: &str) -> u32 {
-    let graph = parse_input(input);
-
-    let mut paths = BTreeMap::from([("you", 1)]);
+fn count_paths(graph: &BTreeMap<&str, BTreeSet<&str>>, start: &str, target: &str) -> u64 {
+    let mut paths = BTreeMap::from([(start, 1)]);
 
     let mut counts = 0;
 
     while !paths.is_empty() {
         for (pos, count) in std::mem::take(&mut paths) {
-            if pos == "out" {
+            if pos == target {
                 counts += count;
             } else if let Some(outs) = graph.get(pos) {
                 for out in outs {
@@ -54,13 +59,44 @@ pub fn part1(input: &str) -> u32 {
             }
         }
     }
-
     counts
 }
 
-pub fn part2(input: &str) -> u32 {
-    let mut iter = parse_input(input);
-    todo!("part2 WIP")
+pub fn part1(input: &str) -> u64 {
+    let graph = parse_input(input);
+
+    count_paths(&graph, "you", "out")
+}
+
+
+pub fn part2(input: &str) -> u64 {
+    let graph = parse_input(input);
+
+    // the graph can't have any cycles otherwise there would be infinit paths from start to goal
+    // hint in task node name dac i.e. directed-acyclic-graph
+
+    let fft_dac = count_paths(&graph, "fft", "dac");
+    let dac_fft = count_paths(&graph, "dac", "fft");
+
+    let (first, second, mid_count) = match (fft_dac, dac_fft) {
+        (0,0) => panic!("impossible"),
+        (0, count) => {
+            ("dac", "fft", count)
+        }
+
+        (count, 0) => {
+            ("fft", "dac", count)
+        }
+        (_,_) => panic!("infinit")
+    };
+
+
+    let start_count = count_paths(&graph, "svr", first);
+    let end_count = count_paths(&graph,  second, "out");
+
+
+
+    start_count * mid_count * end_count
 }
 
 #[test]
@@ -75,10 +111,10 @@ fn part1_full() {
 
 #[test]
 fn part2_example1() {
-    assert_eq!(part2(INPUT_EXAMPLE1), 5);
+    assert_eq!(part2(INPUT_EXAMPLE2), 2);
 }
 
 #[test]
 fn part2_full() {
-    assert_eq!(part2(INPUT), 1262);
+    assert_eq!(part2(INPUT), 545394698933400);
 }
